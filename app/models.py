e256ac2cd301d73a07d678abe797bf26c74c6257
SimonCao <simoncao@techblog.com>
2018-08-21 14:54:22 +0800

    修改了gravatar获取头像的方法，现在可以在右上角准确显示出头像了

--- app/models.py
+++ app/models.py
@@ -49,8 +49,7 @@ class User(UserMixin, db.Model):
         # else:
         #     url = 'http://www.gravatar.com/avatar'
         url = 'https://gravatar.loli.net/avatar'
-        hash = self.avatar_hash or hashlib.md5(
-            self.email.encode('utf-8')).hexdigest()
+        hash = hashlib.md5(self.email.encode('utf-8')).hexdigest()
         return '{url}/{hash}?s={size}&d={default}&r={rating}'.format(
             url=url, hash=hash, size=size, default=default, rating=rating)
 
@@ -242,8 +241,7 @@ class Comment(db.Model):
         # else:
         #     url = 'http://www.gravatar.com/avatar'
         url = 'https://gravatar.loli.net/avatar'
-        hash = self.avatar_hash or hashlib.md5(
-            self.author_email.encode('utf-8')).hexdigest()
+        hash =  hashlib.md5(self.author_email.encode('utf-8')).hexdigest()
         return '{url}/{hash}?s={size}&d={default}&r={rating}'.format(
             url=url, hash=hash, size=size, default=default, rating=rating)
 
